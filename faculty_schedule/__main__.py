3709942a782ae910ebd74e0c09833abb82e1c303
dev-laww <lawrenceallen.tech@gmail.com>
2025-01-17 22:10:38 +0800

    chore: initial project setup

--- faculty_schedule/__main__.py
+++ faculty_schedule/__main__.py
@@ -1,19 +1,17 @@
-from database_opener import database_opener as do
 import tkinter as tk
 from tkinter import messagebox
 
-do.start_apache_mysql() #use this to start mysql and apache
-# do.stop_apache_mysql() #use this to stop mysql and apache
 
+# do.start_apache_mysql() #use this to start mysql and apache
+# do.stop_apache_mysql() #use this to stop mysql and apache
 
 # Function to handle the window close event
 def on_closing():
     # Ask the user for confirmation before closing
     if messagebox.askokcancel("Quit", "Do you want to quit?"):
-        do.stop_apache_mysql() #use this to stop mysql and apache
+        # do.stop_apache_mysql() #use this to stop mysql and apache
         window.destroy()  # Close the window
 
-# Create the main window
 window = tk.Tk()
 
 # Set the window title
@@ -29,5 +27,10 @@ label.pack(pady=20)
 # Bind the close event to the on_closing function
 window.protocol("WM_DELETE_WINDOW", on_closing)
 
-# Run the window
-window.mainloop()
+def main():
+    # Create the main window
+    window.mainloop()
+
+
+if __name__ == "__main__":
+    main()
