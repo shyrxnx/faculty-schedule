1ce9ba117c3e6d05feef532a19b2160891914ade
Shyrine Salvador <84778328+shyrxnx@users.noreply.github.com>
2025-01-26 22:58:12 +0800

    chore: add __init__.py in views/schedules to facilitate module imports

---
+++ faculty_schedule/views/schedules/__init__.py
@@ -0,0 +1,5 @@
+from .schedules_frame import ScheduleFrame
+from .timetable_base_frame import TimetableCanvas
+from .add_schedule_frame import AddSchedFrame
+from .edit_schedule_frame import EditSchedFrame
+from .delete_schedule_frame import DeleteSchedFrame
