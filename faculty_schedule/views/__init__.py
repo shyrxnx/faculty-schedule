f431f6e57acd74c54cf239bae21928631ce39673
Shyrine Salvador <84778328+shyrxnx@users.noreply.github.com>
2025-01-26 23:08:24 +0800

    chore: update __init__.py

--- faculty_schedule/views/__init__.py
+++ faculty_schedule/views/__init__.py
@@ -1,4 +1,7 @@
 from .home import HomeFrame, AddEmployeeFrame, EditEmployeeFrame, DeleteEmployeeFrame
-from .sched_code import SchedCodeFrame
+from .sched_code import SchedCodeFrame, AddSchedCodeFrame, EditSchedCodeFrame, DeleteSchedCodeFrame
+from .schedules import ScheduleFrame, AddSchedFrame, EditSchedFrame, DeleteSchedFrame
 
-__all__ = ["HomeFrame", "AddEmployeeFrame", "EditEmployeeFrame", "DeleteEmployeeFrame", "SchedCodeFrame"]
+__all__ = ["HomeFrame", "AddEmployeeFrame", "EditEmployeeFrame", "DeleteEmployeeFrame", "SchedCodeFrame",
+           "SchedCodeFrame", "AddSchedCodeFrame", "EditSchedCodeFrame", "DeleteSchedCodeFrame",
+           "ScheduleFrame", "AddSchedFrame", "EditSchedFrame", "DeleteSchedFrame"]
--- faculty_schedule/views/schedules/__init__.py
+++ faculty_schedule/views/schedules/__init__.py
@@ -1,4 +1,4 @@
-from .schedules_frame import ScheduleFrame
+from .schedule_frame import ScheduleFrame
 from .timetable_base_frame import TimetableCanvas
 from .add_schedule_frame import AddSchedFrame
 from .edit_schedule_frame import EditSchedFrame
